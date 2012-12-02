8ff940b1e8b9dd856f3ac1dd06ce5b59b48a1a95
Fahad Mansoor <fahadmansoor.ali@gmail.com>
2012-12-02 08:10:36 +0500

    Revert "Initial Commit"
    This reverts commit d6b7d7de83293aef25eaf11c796280652b848f2a.

--- CleanIT Android/gen/urbanUnit/waterAndSanitationHackathon/crowdsourcingandcomplaintmanagementsystem/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package urbanUnit.waterAndSanitationHackathon.crowdsourcingandcomplaintmanagementsystem;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
